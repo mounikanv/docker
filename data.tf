e1b65e49dbc8c0adde66deec8529e42486054f6d
mounikanv <mounikanattama5b3@gmail.com>
2026-03-01 15:12:05 +0530

    docker

--- data.tf
+++ data.tf
@@ -1,11 +1,11 @@
 
 data "aws_ami" "joindevops" {
-  owners           = ["973714476881"]
-  most_recent      = true
+  owners      = ["973714476881"]
+  most_recent = true
 
   filter {
     name   = "name"
-    values = ["RHEL-9-DevOps-Practice"]
+    values = ["RHEL-9-DevOps-Practice*"]  # wildcard allows suffix
   }
 
   filter {
@@ -17,8 +17,4 @@ data "aws_ami" "joindevops" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-}
-
-output "ami_id" {
-  value       = data.aws_ami.joindevops.id
 }
