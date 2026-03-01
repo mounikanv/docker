a2294cb2e8d3f4b8dfe8c421c9e7917fb28df78a
mounikanv <mounikanattama5b3@gmail.com>
2026-03-01 13:46:45 +0530

    docker

--- data.tf
+++ data.tf
@@ -1,11 +1,35 @@
 
-data "aws_ami" "joindevops" {
+/* data "aws_ami" "joindevops" {
   owners           = ["341841617158"]
   most_recent      = true
 
   filter {
     name   = "name"
     values = ["RHEL-9-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+ */
+ provider "aws" {
+  region = "us-east-1"  # change this to your AMI's region
+}
+
+data "aws_ami" "joindevops" {
+  owners      = ["341841617158"]   # your AWS account ID
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["RHEL-9-DevOps-Practice*"]  # wildcard matches any suffix
   }
 
   filter {
